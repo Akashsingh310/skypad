226110c48800c4d699996ce2d93b3126ead31a07
Akash Singh <akash22675@gmail.com>
2023-07-29 14:16:54 +0530

    Fetching Notes from Context

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,26 +1,71 @@
 import NoteContext from "./noteContext";
 import { useState } from "react";
+const NoteState = (props) => {
 
-const NoteState = (props)=>{
-    const s1 = {
-        "name": "Akash",
-        "class": "12"
-    }
-    const [state, setState] = useState(s1);
+  const notesInitial =
+    [
+      {
+        "_id": "64bb6d5181e298f343ab988a",
+        "user": "64ba1f0534750efca69b90d4",
+        "title": "Mytitle",
+        "description": "Please wake up early",
+        "tag": "personal",
+        "date": "2023-07-22T05:46:57.499Z",
+        "__v": 0
+      },
+      {
+        "_id": "64bb6dd4964f02393cfce496",
+        "user": "64ba1f0534750efca69b90d4",
+        "title": "Mytitle",
+        "description": "Please wake up early",
+        "tag": "personal",
+        "date": "2023-07-22T05:49:08.094Z",
+        "__v": 0
+      },
+      {
+        "_id": "64bbbcebbd1b9ff3c3c9b026",
+        "user": "64ba1f0534750efca69b90d4",
+        "title": "Mytitle",
+        "description": "Please wake up early",
+        "tag": "personal",
+        "date": "2023-07-22T11:26:35.467Z",
+        "__v": 0
+      },
+      {
+        "_id": "64bbbcebbd1b9ff3c3c9b026",
+        "user": "64ba1f0534750efca69b90d4",
+        "title": "Mytitle",
+        "description": "Please wake up early",
+        "tag": "personal",
+        "date": "2023-07-22T11:26:35.467Z",
+        "__v": 0
+      }, {
+        "_id": "64bb6dd4964f02393cfce496",
+        "user": "64ba1f0534750efca69b90d4",
+        "title": "Mytitle",
+        "description": "Please wake up early",
+        "tag": "personal",
+        "date": "2023-07-22T05:49:08.094Z",
+        "__v": 0
+      },
+      {
+        "_id": "64bbbcebbd1b9ff3c3c9b026",
+        "user": "64ba1f0534750efca69b90d4",
+        "title": "Mytitle",
+        "description": "Please wake up early",
+        "tag": "personal",
+        "date": "2023-07-22T11:26:35.467Z",
+        "__v": 0
+      },
+    ]
 
-    const update = ()=>{
-        setTimeout(() => {
-            setState({
-                "name": "Ashutosh",
-                "class": "10"
-            })
-        }, 1000);
-    }
-    return (
-        <NoteContext.Provider value={{state:state, update:update}}>
-            {props.children}
-        </NoteContext.Provider>
-    )
+  const [notes, setNotes] = useState(notesInitial)
+
+  return (
+    <NoteContext.Provider value={{ notes, setNotes }}>
+      {props.children}
+    </NoteContext.Provider>
+  )
 }
 
 export default NoteState;
